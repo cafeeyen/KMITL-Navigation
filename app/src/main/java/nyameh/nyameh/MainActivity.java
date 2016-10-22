13cb643809017d26b171aedf182178bebcd53a4c
cafeeyen <s7070113@kmitl.ac.th>
2016-10-22 22:18:45 +0700

    QRNS v0.2
    Add alert box in QR Scan

--- app/src/main/java/nyameh/nyameh/MainActivity.java
+++ app/src/main/java/nyameh/nyameh/MainActivity.java
@@ -6,17 +6,13 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import com.google.zxing.Result;
-import com.google.zxing.integration.android.IntentIntegrator;
-import com.google.zxing.integration.android.IntentResult;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
-public class MainActivity extends Activity implements ZXingScannerView.ResultHandler {
+public class MainActivity extends Activity implements ZXingScannerView.ResultHandler
+{
     public final static String EXTRA_MESSAGE = "Nyameh.MESSAGE";
     private ZXingScannerView mScannerView;
 
@@ -48,23 +44,28 @@ public class MainActivity extends Activity implements ZXingScannerView.ResultHan
     public void handleResult(Result rawResult)
     {
         String result = rawResult.getText();
-        if(result != null){
-            if(QRCode.checkQRResult(result)){ //can pass to next page
+        if(result != null)
+        {
+            if(QRCode.checkQRResult(result))
+            { //can pass to next page
                 Intent showText = new Intent(this, ScanQRCodeActivity.class);
                 showText.putExtra(EXTRA_MESSAGE, result);
                 startActivity(showText);
             }
-            else{ //pop up alert
+            else
+            { //pop up alert
                 AlertDialog.Builder builder = new AlertDialog.Builder(this);
                 builder.setTitle("Wrong QR Code");
                 builder.setMessage("Result of This code is \n" + result);
                 AlertDialog alert1 = builder.create();
                 alert1.show();
+                mScannerView.resumeCameraPreview(this);
             }
         }
     }
     @Override
-    public void onPause() {
+    public void onPause()
+    {
         super.onPause();
         mScannerView.stopCamera(); // Stop camera on pause
     }
